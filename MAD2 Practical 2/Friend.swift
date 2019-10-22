45986aae8e6443c169a5cf1b4dc4634f11d14fd8
shin <acrylicblaze@gmail.com>
2019-10-22 14:55:06 +0800

    Test initial commit

---
+++ MAD2 Practical 2/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  MAD2 Practical 2
+//
+//  Created by MAD2_P03 on 22/10/19.
+//  Copyright © 2019 Shin. All rights reserved.
+//
+
+class Friend {
+    var firstname: String= ""
+    
+    init(firstname:String) {
+        self.firstname = firstname
+    }
+}
---
+++ MAD2 Practical 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAD2 Practical 2
+//
+//  Created by MAD2_P03 on 22/10/19.
+//  Copyright © 2019 Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
