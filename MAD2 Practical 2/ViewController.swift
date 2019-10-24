39f50741c1623a0eed4e23b292a7cd5d77ec5d25
shin <acrylicblaze@gmail.com>
2019-10-24 15:03:12 +0800

    Practical 2 Completed
    Application is able to display Friends' names on a label when button is clicked.

--- MAD2 Practical 2/ViewController.swift
+++ MAD2 Practical 2/ViewController.swift
@@ -9,33 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var txtFriend: UILabel!
     
     var friendList:Array<Friend> = []
     
-    func initFriends() {
-        friendList.append(Friend(firstname: "Ale", lastname: "Coal", age: 10));
-        friendList.append(Friend(firstname: "ble", lastname: "Coal", age: 10));
-        friendList.append(Friend(firstname: "cle", lastname: "Coal", age: 10));
-        friendList.append(Friend(firstname: "dle", lastname: "Coal", age: 10));
-        friendList.append(Friend(firstname: "ele", lastname: "Coal", age: 10))
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initFriends()
+        displayFriendsUnder20()
+        findAverageAge()
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        
+    }
+    
+
+    func initFriends() {
+        friendList.append(Friend(firstname: "Ale", lastname: "Coal", age: 15));
+        friendList.append(Friend(firstname: "Billy", lastname: "Coal", age: 12));
+        friendList.append(Friend(firstname: "Clear", lastname: "Coal", age: 13));
+        friendList.append(Friend(firstname: "Del", lastname: "Coal", age: 11));
+        friendList.append(Friend(firstname: "Elle", lastname: "Coal", age: 10))
+    }
+    
     func displayFriends() {
+        txtFriend.text = ""
         for friend in friendList{
-            print(friend.firstName,friend.lastName,friend.age)
+            print("\(friend.firstName) \(friend.lastName) \(friend.age)")
+            txtFriend.text = txtFriend.text! + "\(friend.firstName) \(friend.lastName), \(friend.age)" + "\n"
+            
         }
     }
     
     func displayFriendsUnder20(){
         for friend in friendList{
             if friend.age < 20 {
-               print(friend.firstName,friend.lastName,friend.age)
+               print("\(friend.firstName) \(friend.lastName) is underage.")
             }
         }
     }
@@ -46,6 +60,9 @@ class ViewController: UIViewController {
             sum += friend.age
         }
         return Double(sum / friendList.count)
+    }
+    @IBAction func btnClick(_ sender: Any) {
+        displayFriends();
     }
 }
 
